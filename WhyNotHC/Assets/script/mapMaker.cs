0e3f5c5b55772791a37a7334250463468325009a
kangyoongu <rkas7890@gmail.com>
2023-03-19 23:50:21 +0900

    건물생성

--- WhyNotHC/Assets/script/mapMaker.cs
+++ WhyNotHC/Assets/script/mapMaker.cs
@@ -8,13 +8,20 @@ public class mapMaker : MonoBehaviour
     public Transform player;
     public GameObject build;
     int z = -3;
+    int l = -75;
     public Text sc;
+    public GameObject low;
     void Update()
     {
         if(player.position.z/25 > z)
         {
             z += 1;
-            Instantiate(build, new Vector3(Random.Range(-10f, 10), -32.8622f, (z + 3) * 25), Quaternion.identity);
+            Instantiate(build, new Vector3(Random.Range(-10f, 10), 0, (z + 3) * 25), Quaternion.identity);
+        }
+        if (player.position.z/2 > l)
+        {
+            l += 1;
+            Instantiate(low, new Vector3(Random.Range(-15f, 15), Random.Range(-3, 3), (l+37) * 2), Quaternion.identity);
         }
         sc.text = (z-1).ToString("0");
     }
