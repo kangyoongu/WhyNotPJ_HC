c109d8a07437fc9096defb5fb646d950c0e6a7cf
kangyoongu <rkas7890@gmail.com>
2023-03-29 18:35:52 +0900

    combo

--- WhyNotHC/Assets/script/oilManager.cs
+++ WhyNotHC/Assets/script/oilManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class OilManager : MonoBehaviour
 {
     public Image bar;//오일 바
@@ -9,6 +10,8 @@ public class OilManager : MonoBehaviour
     public Height he;
     public int score = 0;//게임 점수
     public Text sc;//게임 점수 텍스트
+    int combo = 0;
+    public TextMeshProUGUI combo_text;
     void Update()
     {
         if(landing == false)//떠있다면 오일 깎는다
@@ -20,21 +23,38 @@ public class OilManager : MonoBehaviour
         if (collision.gameObject.tag == "building")
         {
             landing = true;
-            bar.fillAmount += 0.3f;//착륙하면 연료채움
             if (collision.transform.position.z >= -0.4)//착륙 얼마나 중앙에 가까운지에 따라 점수 줌
             {
                 if (Vector3.Distance(transform.position, collision.transform.position) <= 1.6f)
                 {
                     score += 3;
+                    combo += 1;
+                    if (combo % 5 == 0)
+                    {
+                        score += 1;
+                    }
+                    combo_text.text = combo.ToString() + " combo";
                 }
                 else if (Vector3.Distance(transform.position, collision.transform.position) <= 2.5f)
                 {
                     score += 2;
+                    combo = 0;
+                    combo_text.text = "";
                 }
                 else
                 {
                     score += 1;
+                    combo = 0;
+                    combo_text.text = "";
                 }
+            }
+            if (combo <= 7)
+            {
+                bar.fillAmount += 0.3f * 1 + (combo * 0.1f); //콤보에 따라 연료 줌
+            }
+            else
+            {
+                bar.fillAmount += 0.54f;
             }
         }
     }
