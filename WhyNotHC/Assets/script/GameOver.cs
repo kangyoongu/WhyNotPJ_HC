1f4f722acb7a67d378ece50cd240bbfd2cd713f0
kangyoongu <rkas7890@gmail.com>
2023-03-30 20:09:24 +0900

    custom2

--- WhyNotHC/Assets/script/GameOver.cs
+++ WhyNotHC/Assets/script/GameOver.cs
@@ -17,9 +17,11 @@ public class GameOver : MonoBehaviour
     public OilManager oil;
     public GameObject[] main;
     public GameObject[] play;
+    public GameObject[] custom;
     public GameObject setting;
     public Transform gmp;
     public ItemSpawn item;
+    public Transform point;
     void Start()
     {
         if (!PlayerPrefs.HasKey("best"))
@@ -49,6 +51,10 @@ public class GameOver : MonoBehaviour
         {
             timer = 0;
         }
+        if(custom[0].activeSelf == true)
+        {
+            point.localRotation = Quaternion.Euler(-15, point.localEulerAngles.y + Time.deltaTime * 60, 0);
+        }
     }
     public void OnClickrestart()//다시시작 버튼을 눌렀을 때
     {
@@ -65,7 +71,7 @@ public class GameOver : MonoBehaviour
         back.SetActive(false);
         oil.score = 0;
         gmp.position = new Vector3(0.02899998f, -1.599503f, -0.4820083f);
-        GameObject[] gold = GameObject.FindGameObjectsWithTag("gold");
+        GameObject[] gold = GameObject.FindGameObjectsWithTag("Coin");
         for(int i = 1; i < gold.Length; i++)
         {
             Destroy(gold[i]);
@@ -102,6 +108,31 @@ public class GameOver : MonoBehaviour
         }
         setting.SetActive(false);
         OnClickrestart();
+    }
+    public void OnCLickCustom()//커스텀창 열기
+    {
+        point.localPosition = new Vector3(0, -7, 0);
+        for (int i = 0; i < main.Length; i++)
+        {
+            main[i].SetActive(false);
+        }
+        for (int i = 0; i < custom.Length; i++)
+        {
+            custom[i].SetActive(true);
+        }
+    }
+    public void OnCLickCustom_out()//커스텀창 닫기
+    {
+        point.localPosition = new Vector3(0, 0, 0);
+        point.localRotation = Quaternion.Euler(0, 0, 0);
+        for (int i = 0; i < main.Length; i++)
+        {
+            main[i].SetActive(true);
+        }
+        for (int i = 0; i < custom.Length; i++)
+        {
+            custom[i].SetActive(false);
+        }
     }
     public void OnClickSetting()//설정버튼 눌렀을 때
     {
