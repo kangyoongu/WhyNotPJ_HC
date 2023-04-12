93fd0f1e398e8f6977e1cd73794c97bcf0980976
kangyoongu <rkas7890@gmail.com>
2023-04-12 18:20:05 +0900

    0412

--- WhyNotHC/Assets/script/CustomManager.cs
+++ WhyNotHC/Assets/script/CustomManager.cs
@@ -7,18 +7,18 @@ public class CustomManager : MonoBehaviour
 {
     public Material[] mat;
     public MeshRenderer play;
-    public TextMeshProUGUI mil;
     public TextMeshProUGUI rain;
+    public TextMeshProUGUI coin;
     private void Start()
     {
         if (!PlayerPrefs.HasKey("rainbuy"))
         {
-            PlayerPrefs.SetInt("milbuy", 0);
             PlayerPrefs.SetInt("rainbuy", 0);
+            PlayerPrefs.SetInt("onmat", 0);
         }
+        play.material = mat[PlayerPrefs.GetInt("onmat")];
         if (PlayerPrefs.GetInt("rainbuy") == 1)
         {
-            mil.text = "군용 헬기 보유";
             rain.text = "무지개 헬기 보유";
         }
     }
@@ -28,30 +28,24 @@ public class CustomManager : MonoBehaviour
     }
     public void OnClickMil()
     {
-        if (PlayerPrefs.GetInt("coin") >= 100)
-        {
-            play.material = mat[0];
-            PlayerPrefs.SetInt("coin", PlayerPrefs.GetInt("coin") - 100);
-            PlayerPrefs.SetInt("milbuy", 1);
-            mil.text = "군용 헬기 보유";
-        }
-        if (PlayerPrefs.GetInt("milbuy") == 1)
-        {
-            play.material = mat[0];
-        }
+        play.material = mat[0];
+        PlayerPrefs.SetInt("onmat", 0);
     }
     public void OnClickRain()
     {
-        if (PlayerPrefs.GetInt("coin") >= 200)
+        if (PlayerPrefs.GetInt("rainbuy") == 1)
+        {
+            play.material = mat[1];
+            PlayerPrefs.SetInt("onmat", 1);
+        }
+        else if (PlayerPrefs.GetInt("coin") >= 200)
         {
             play.material = mat[1];
             PlayerPrefs.SetInt("coin", PlayerPrefs.GetInt("coin") - 200);
             PlayerPrefs.SetInt("rainbuy", 1);
             rain.text = "무지개 헬기 보유";
-        }
-        if (PlayerPrefs.GetInt("rainbuy") == 1)
-        {
-            play.material = mat[1];
+            PlayerPrefs.SetInt("onmat", 1);
+            coin.text = "coin : " + PlayerPrefs.GetInt("coin");
         }
     }
 }
