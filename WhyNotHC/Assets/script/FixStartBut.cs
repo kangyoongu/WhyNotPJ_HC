e9ee538763ef0517b9a353ddb2fe6d194807acc7
kangyoongu <rkas7890@gmail.com>
2023-04-20 19:43:54 +0900

    dotween

---
+++ WhyNotHC/Assets/script/FixStartBut.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixStartBut : MonoBehaviour
+{
+    float ratio = 0;
+    void Start()
+    {
+        
+    }
+}
