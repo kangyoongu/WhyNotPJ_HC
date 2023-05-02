8593bcd79e2026d76cf5d2dafc8ff9ea4313bb8b
leo82380 <tkdrb062@gmail.com>
2023-05-02 12:58:09 +0900

    Bomb scale BIG

--- WhyNotHC/Assets/Item/Scripts/ScalePlus.cs
+++ WhyNotHC/Assets/Item/Scripts/ScalePlus.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScalePlus : MonoBehaviour
 {
+    [SerializeField] Image plusTimer;
   public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Plus")//헬기 사이즈 키우기
         {
+            plusTimer.fillAmount -= 0.1f;
             other.gameObject.SetActive(false);
             this.transform.localScale += new Vector3(.2f, .2f, .2f);
             Invoke("MinusSize", 10);
